dab61c9557dc21443c5170f346d03a61ddad1678
Vasco Conde <vasconde@gmail.com>
2012-08-11 13:38:23 +0100

    GPL license v3

--- nivela.py
+++ nivela.py
@@ -6,9 +6,25 @@
 # |_| \_|_| \_/ \___|_|\__,_| /_/    \_\ .__/| .__/ 
 #                                      | |   | |    
 #                                      |_|   |_|
-# Leveling computation and adjustment
+
+#  Nivela -- Leveling computation and adjustment
+#  Copyright (C) 2012  Vasco Conde <vasconde@gmail.com>
 #
-# Author: Vasco Conde (vasconde)
+#  This file is part of the Nivela.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA	
 
 # Nivela Modules
 import IOData
